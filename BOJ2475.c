223003b5998f08db3de382c1412a5c239638e281
alstj0127 <end020127@naver.com>
2021-11-11 01:45:57 +0900

    Create BOJ2475.c

---
+++ BOJ2475.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(void) {
+    int n, sum = 0;
+    for (int i = 0; i < 5; i++) {
+        scanf("%d", &n);
+        sum = sum + n * n;
+    }
+    printf("%d", sum % 10);
+}
