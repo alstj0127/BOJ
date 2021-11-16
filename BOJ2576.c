c134d17fbae492092f19739c443931fb8902fc99
alstj0127 <end020127@naver.com>
2021-11-17 02:14:57 +0900

    BOJ2576

--- BOJ2576.c
+++ BOJ2576.c
@@ -1,30 +1,18 @@
 #include<stdio.h>
 
 int main(void) {
-    int tmp, sum = 0;
-    int arr[7];
-    int arr2[7] = {0, 0, 0, 0, 0, 0, 0};
+    int tmp, min = 100, sum = 0;
     for (int i = 0; i < 7; i++) {
-        scanf("%d", &arr[i]);
-        if (arr[i] % 2 == 1) arr2[i] = arr[i];
-    }
-    for (int i = 0; i < 7; i++) {
-        for (int j = i; j < 7; j++) {
-            if (arr2[i] > arr2[j]) {
-                tmp = arr2[i];
-                arr2[i] = arr2[j];
-                arr2[j] = tmp;
-            }
+        scanf("%d", &tmp);
+        if (tmp%2!=0){
+            sum+=tmp;
+            if(min>tmp) min = tmp;
         }
     }
-    if (sum!=0){
-        for (int i = 6; arr2[i] != 0; i--) {
-            sum += arr2[i];
-        }
-        printf("%d\n", sum);
-        int check=0;
-        for(; arr2[check]==0;check++){}
-        printf("%d",arr2[check]);
-    } else printf("-1");
+    if(sum==0){
+        printf("-1");
+        return 0;
+    }
+    else    printf("%d\n%d", sum, min);
     return 0;
 }
